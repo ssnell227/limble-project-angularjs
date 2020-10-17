d5d3e0aa51cc0a215af0457133c2e4e04b818d3e
ssnell227 <ssnell227@gmail.com>
2020-10-16 18:58:02 -0600

    arrow keys select matched user

--- app/userSelect/user-select.component.js
+++ app/userSelect/user-select.component.js
@@ -6,14 +6,15 @@ function userSelectController($scope, $element) {
     { 'userID': 3, 'name': 'Bryan' },
     { 'userID': 4, 'name': 'Gabbey' },
   ];
-
   $scope.filteredUsers = ctrl.users
   $scope.showUserSelect = false
   $scope.matchedUser = $scope.filteredUsers[0]
 
   $scope.$on('textToUserSelect', (events, text, key) => {
+    if (key !== 'Enter' && key !== 'ArrowUp' && key !== 'ArrowDown') {
+      $scope.filterUsers(text)
+    }
     $scope.toggleUserSelect(text)
-    $scope.filterUsers(text)
     $scope.watchKey(key)
   })
 
@@ -32,20 +33,36 @@ function userSelectController($scope, $element) {
 
   $scope.filterUsers = function (text) {
     let nameSubStr = text.substring(text.lastIndexOf('@') + 1).toLowerCase()
-    const filteredUsers = [...ctrl.users].filter(user => user.name.toLowerCase().includes(nameSubStr))
+    const filteredUsers = [...ctrl.users]
+      .filter(user => user.name.toLowerCase().includes(nameSubStr))
+      .map((item, index) => {
+        return index === 0 ? {...item, match: true} : {...item, match: false}
+      })
     if (filteredUsers.length) {
       $scope.filteredUsers = filteredUsers
+      $scope.matchedUser = filteredUsers[0]
     } else {
       $scope.filteredUsers = ctrl.users
+      $scope.matchedUser = ctrl.users[0]
     }
     $scope.$apply()
-    console.log($scope.filteredUsers)
   }
 
   $scope.watchKey = function (key) {
+    const currentMatchIndex = $scope.filteredUsers.findIndex(user => user.match)
     if (key === 'Enter' && $scope.showUserSelect) {
-      console.log($scope.filteredUsers)
       $scope.$emit('sendMatchedUser', $scope.matchedUser)
+      $scope.showUserSelect = false
+      $scope.$apply()
+    } else if (key === 'ArrowDown') {
+      //change the class to the next item in the list and change matched user to the next item in the list
+      $scope.filteredUsers = $scope.filteredUsers.map((item, index) => {
+        return index === currentMatchIndex +1 ? {...item, match: true} : {...item, match: false}
+      })
+    } else if (key === 'ArrowUp') {
+      $scope.filteredUsers = $scope.filteredUsers.map((item, index) => {
+        return index === currentMatchIndex - 1 ? {...item, match: true} : {...item, match: false}
+      })
     }
   }
 
@@ -56,14 +73,14 @@ angular.
   module('myApp').
   component('userSelect', {
     template:
-      `<div class="control">
-    <div ng-hide='!showUserSelect' class="select is-mulitple" >
-      <select multiple size="4">
-        <option ng-repeat='user in filteredUsers'>
-            {{user.name}}
-        </option>
-      </select>
-    </div>
-  </div>`,
+      `<div ng-hide='!showUserSelect' class="dropdown is-active" >
+        <div class='dropdown-menu'>
+          <div class='dropdown-content'>
+            <div class='dropdown-item' ng-repeat='user in filteredUsers'>
+              {{user.name}}
+            </div>
+          </div>
+        </div>
+      </div>`,
     controller: userSelectController
   })
