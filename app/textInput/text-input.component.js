924640372ab64ba7fd54b0fae5066a8e52eac278
ssnell227 <ssnell227@gmail.com>
2020-10-17 21:26:55 -0600

    update input width

--- app/textInput/text-input.component.js
+++ app/textInput/text-input.component.js
@@ -38,12 +38,12 @@ angular.
     component('textInput', {
         template:
     `<input
-    ng-trim='false'
     id='text-input'
     class="input"
     ng-model='$ctrl.inputText'
     ng-value='$ctrl.currentText'
     ng-change='$ctrl.updateText({text: $ctrl.inputText})'
+    ng-trim='false'
     ></input>`,
         controller: textInputController,
         bindings: {
